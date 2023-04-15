98d13299ffb31b3cbe86d10b1fcc5fa485a6203b
Ashoemaker_tech <andrew.shoemaker9@gmail.com>
2023-04-15 01:00:17 -0500

    init

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import TwitterLayout from '@/Layouts/TwitterLayout.vue';
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+<TwitterLayout>
+<div class="text-red-600">
+    Welcome
+</div>
+</TwitterLayout>
+   </template>
+
+<style>
+
+</style>
